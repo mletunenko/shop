b3daecbecd226ba30de6aa4223504743274676c9
maria <@.com>
2022-05-16 15:43:04 +0400

    added signal for bucket creating +

---
+++ marketplace/signals.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from marketplace.models import Bucket
+
+
+@receiver(post_save, sender=User)
+def post_save_user(sender, instance,  created, **kwargs):
+    if created:
+        Bucket.objects.create(user=instance)
+
+
+
+
+
